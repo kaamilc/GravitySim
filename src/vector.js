3bacf656b151b4bcbb4624cbbf53486fc38aae5f
mkrej <mkrej>
2023-11-29 12:26:12 +0100

    clean

--- src/vector.js
+++ src/vector.js
@@ -5,62 +5,62 @@ const ToScreenMul = 8
 const TimeStep = 0.01
 
 class Vector {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x || 0;
         this.y = y || 0;
         this.update();
     }
 
-    getDirection(){
+    getDirection() {
         return Math.atan2(this.y, this.x);
     }
 
-    getMagnitude(){
+    getMagnitude() {
         return Math.sqrt(Math.max(this.x * this.x + this.y * this.y));
     }
 
-    addtoVector(v2){
+    addtoVector(v2) {
         this.x += v2.x;
         this.y += v2.y;
         this.update();
     }
 
-    static add(v1, v2){
+    static add(v1, v2) {
         return new Vector(v1.x + v2.x, v1.y + v2.y)
     }
 
-    substractfromVector(v2){
+    substractfromVector(v2) {
         this.x -= v2.x;
         this.y -= v2.y;
         this.update();
     }
 
-    multiplyVector(scalar){
+    multiplyVector(scalar) {
         this.x *= scalar;
         this.y *= scalar;
         this.update();
     }
 
-    static multiply(v1, scalar){
-        return new Vector(v1.x * scalar , v1.y * scalar)
+    static multiply(v1, scalar) {
+        return new Vector(v1.x * scalar, v1.y * scalar)
     }
 
-    divideVector(scalar){
+    divideVector(scalar) {
         this.x /= scalar;
         this.y /= scalar;
         this.update();
     }
 
-    static divide(v1, scalar){
-        return new Vector(v1.x / scalar , v1.y / scalar)
+    static divide(v1, scalar) {
+        return new Vector(v1.x / scalar, v1.y / scalar)
     }
 
-    update(){
+    update() {
         this.direction = this.getDirection()
         this.magnitude = this.getMagnitude()
     }
 
-    static CreateFromMagDir(magnitude, direction){
+    static CreateFromMagDir(magnitude, direction) {
         var res = new Vector()
         res.magnitude = magnitude;
         res.direction = direction;
@@ -71,8 +71,8 @@ class Vector {
 }
 
 
-class GravityfallProg{
-    constructor(){
+class GravityfallProg {
+    constructor() {
         this.GConst = 1e+0;
         this.bgimg = new Image();
         this.imgLoaded = false
@@ -125,38 +125,28 @@ class GravityfallProg{
     }
 
     iter() {
-       // while(t < 10){
-            ctx.fillStyle = "black";
-            if(ifcheck() != true){
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-            }
-            for(var i = 0; i < StepsPerFarme; i++){
-                this.points = this.Rk4Step(this.points, this.dt);
-            }
-            
-            // ctx.beginPath()
-            // ctx.arc(this.xtoscreen(this.points[0].Position.x), this.ytoscreen(this.points[0].Position.y), 50, 0, 2 * Math.PI);
-            // ctx.fillStyle = "rgb(223, 50, 2)"
-            // ctx.fill()
-            // ctx.beginPath()
-            // ctx.arc(this.xtoscreen(this.points[1].Position.x), this.ytoscreen(this.points[1].Position.y), 5, 0, 2 * Math.PI);
-            // ctx.fillStyle = "rgb(36, 171, 186)"
-            // ctx.fill()
-            // ctx.beginPath()
-            // ctx.arc(this.xtoscreen(this.points[2].Position.x), this.ytoscreen(this.points[2].Position.y), 5, 0, 2 * Math.PI);
-            // ctx.fillStyle = "rgb(67, 67, 67)"
-            // ctx.fill()
-            this.points.forEach(point => this.drawPoint(point));
-            if (false){
-                console.log(this.points[0].Position.x + ";" + this.points[0].Position.y)
-                console.log(this.points[1].Position.x + ";" + this.points[1].Position.y)           
-                console.log(this.points[2].Position.x + ";" + this.points[2].Position.y)
-            }
-            this.t += this.dt;
+
+        ctx.fillStyle = "black";
+        if (ifcheck() != true) {
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        }
+        for (var i = 0; i < StepsPerFarme; i++) {
+            this.points = this.Rk4Step(this.points, this.dt);
+        }
+
+        this.points.forEach(point => this.drawPoint(point));
+
+        if (false) {
+            console.log(this.points[0].Position.x + ";" + this.points[0].Position.y)
+            console.log(this.points[1].Position.x + ";" + this.points[1].Position.y)
+            console.log(this.points[2].Position.x + ";" + this.points[2].Position.y)
+        }
+
+        this.t += this.dt;
     }
 
 
-    Rk4Step(points, h){
+    Rk4Step(points, h) {
         const k1 = this.f(points);
         const sys2 = this.step(points, points, k1, h * 0.5);
         const k2 = this.f(sys2);
@@ -174,15 +164,16 @@ class GravityfallProg{
             .map(el => Vector.multiply(el, h / 6));
 
         const dr0 = [
-                points.map(el => el.Velocity),
-                sys2.map(el => Vector.multiply(el.Velocity, 2)),
-                sys3.map(el => Vector.multiply(el.Velocity, 2)),
-                sys4.map(el => el.Velocity)
-            ]
+            points.map(el => el.Velocity),
+            sys2.map(el => Vector.multiply(el.Velocity, 2)),
+            sys3.map(el => Vector.multiply(el.Velocity, 2)),
+            sys4.map(el => el.Velocity)
+        ]
+
         var dr = dr0
             .reduce((ki, kii) => this.addtb(ki, kii))
             .map(el => Vector.multiply(el, h / 6));
-            
+
         return this.step_in(points, dv, dr);
     }
 
@@ -191,12 +182,11 @@ class GravityfallProg{
     }
 
     addtb(tbv1, tbv2) {
-        // return inp.map(el => Vector.add(el, d));
-        let zipped = []
-        for(let i = 0; i < tbv1.length; i++){
-              zipped.push(Vector.add(tbv1[i], tbv2[i]))
+        let result = []
+        for (let i = 0; i < tbv1.length; i++) {
+            result.push(Vector.add(tbv1[i], tbv2[i]))
         }
-        return zipped;    
+        return result;
     }
 
     f(system) {
@@ -204,14 +194,8 @@ class GravityfallProg{
     }
 
     step(toMove, state, acc, h) {
-        // return toMove.map((el, i) => {
-        //     return{
-        //         Mass: el.Mass,
-        //         Velocity: el.Velocity + acc[i],
-        //         Position: el.Position + state[i] * h
-        //     };
-        // });
-        return this.step_in(toMove, this.multb(h, acc), this.multb(h, state.map(el=>el.Velocity)) )
+
+        return this.step_in(toMove, this.multb(h, acc), this.multb(h, state.map(el => el.Velocity)))
     }
 
     step_in(system, dv, dr) {
@@ -219,30 +203,30 @@ class GravityfallProg{
         let result = []
         for (let i = 0; i < system.length; i++) {
             var el = system[i]
-            result.push(new Particle(el.Mass, Vector.add(el.Position, dr[i]), Vector.add(el.Velocity, dv[i]), el.Color, el.Radius))
+            result.push(
+                new Particle(
+                    el.Mass,
+                    Vector.add(el.Position, dr[i]),
+                    Vector.add(el.Velocity, dv[i]),
+                    el.Color,
+                    el.Radius
+                ))
         }
-
         return result;
-        // return toMove.map((el, i) => {
-        //     return{
-        //         Mass: el.Mass,
-        //         Velocity: el.Velocity + acc[i],
-        //         Position: el.Position + state[i] * h
-        //     };
-        // });
+
     }
 
-    eulerStep(points, dt) {
-        return this.eulerStep(points, points, dt)
-    }
+    // eulerStep(points, dt) {
+    //     return this.eulerStep(points, points, dt)
+    // }
 
-    eulerStep(toMove, system, dt) {
-        if(toMove.length !== system.length){
-            throw new Error(`EulerStep toMove.Length != system.length ${toMove.length} ${system.length}`)
-        }
-        const hf = this.multb(dt, this.f(system));
-        return this.step(toMove, hf, system.map((sys, i) => dt * (sys.Velocity + hf[i])));
-    }
+    // eulerStep(toMove, system, dt) {
+    //     if (toMove.length !== system.length) {
+    //         throw new Error(`EulerStep toMove.Length != system.length ${toMove.length} ${system.length}`)
+    //     }
+    //     const hf = this.multb(dt, this.f(system));
+    //     return this.step(toMove, hf, system.map((sys, i) => dt * (sys.Velocity + hf[i])));
+    // }
 
     accelerationComp(me, system) {
         const allOther = system.filter(el => el !== me);
@@ -255,27 +239,29 @@ class GravityfallProg{
         }).reduce((v1, v2) => Vector.add(v1, v2))
         return Vector.multiply(force, 1.0 / me.Mass);
     }
+
     xtoscreen(l) {
         return Math.round(l * ToScreenMul + canvas.width / 2)
     }
-    
+
     ytoscreen(l) {
         return Math.round(l * ToScreenMul + canvas.height / 2)
     }
 
-    drawPoint(point){
+    drawPoint(point) {
         ctx.beginPath()
         ctx.arc(this.xtoscreen(point.Position.x), this.ytoscreen(point.Position.y), point.Radius, 0, 2 * Math.PI);
         ctx.fillStyle = point.Color;
         ctx.fill()
     }
-    print(){
+
+    print() {
         this.points.forEach(point => console.log(point));
     }
 }
 
 class Particle {
-    constructor(mass, position, velocity, color, radius){
+    constructor(mass, position, velocity, color, radius) {
         this.Mass = mass;
         this.Position = position;
         this.Velocity = velocity;
@@ -284,28 +270,27 @@ class Particle {
     }
 }
 
-var newprog = new GravityfallProg();
-//newprog.run()
+var prog = new GravityfallProg();
 
 window.onload = function () {
     // document.addEventListener("keydown", keyDown);
     // document.addEventListener("keyup", keyUp);
-    // //setInterval(update, 10);
+    // setInterval(update, 10);
     window.requestAnimationFrame(update)
 }
 
 function update() {
-    newprog.iter()
+    prog.iter()
     window.requestAnimationFrame(update)
 }
 
-function button(){
-    newprog.print()
+function button() {
+    prog.print()
 }
 
-function ifcheck(){
+function ifcheck() {
     var cb = document.getElementById("check")
-    if(cb.checked){
+    if (cb.checked) {
         return true
     }
 }
